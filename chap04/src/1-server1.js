6479de1e3f11511d5b4a99c93e3644f1d95fa79a
assu10 <juhyun10.lee@gmail.com>
2021-11-30 14:09:57 +0900

    Node.js - http 모듈로 서버 생성  - 1. 요청과 응답

---
+++ chap04/src/1-server1.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.write('hello');
+    res.end('END');
+  })
+  .listen(8080, () => {
+    // 서버 연결
+    console.log('waiting 8080 port...');
+  });
