9670e81a5f4c17ebca2f85118175e5effbf0b3ab
assu10 <juhyun10.lee@gmail.com>
2021-12-03 15:28:19 +0900

    Node.js - Express (2): 라우터, 템플릿 엔진  - 1.3. 라우터 주소 표현

--- chap06-routes/routes/index.js
+++ chap06-routes/routes/index.js
@@ -7,7 +7,8 @@ const router = express.Router();
   res.send('Hello, Express');
 });*/
 
-router.get(
+// 1.2. next('route')
+/*router.get(
   '/',
   (req, res, next) => {
     res.send('hello1~'); // [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
@@ -31,6 +32,19 @@ router.get('/aa', (req, res) => {
 router.get('/', (req, res) => {
   console.log('실행됨');
   res.send('hello2~');
+});*/
+
+// 1.3. 라우터 주소 표현
+router.get('/me/:id', (req, res) => {
+  res.send('me~');
+  console.log('req.params: ', req.params);
+  console.log('req.query: ', req.query);
+});
+
+router.get('/me/haha', (req, res) => {
+  res.send('실행되지 않음');
+  console.log('req.params: ', req.params);
+  console.log('req.query: ', req.query);
 });
 
 module.exports = router;
