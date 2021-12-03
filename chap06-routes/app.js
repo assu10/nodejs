41b72145df6cff8322ce098c47f578d8c240c3f0
assu10 <juhyun10.lee@gmail.com>
2021-12-03 14:51:04 +0900

    Node.js - Express (2): 라우터, 템플릿 엔진  - 1. Router 객체로 라우팅 분리

---
+++ chap06-routes/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const indexRouter = require('./routes'); // index.js 는 생략 가능
+const userRouter = require('./routes/users');
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: 'session-cookie',
+  }),
+);
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+// 위 라우터에 매핑되지 않으면 404 에러
+app.use((req, res, next) => {
+  res.status(404).send('NOT FOUND.');
+});
+
+// 에러 핸들러
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
